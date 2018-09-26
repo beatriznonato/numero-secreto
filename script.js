23b0fb169c3af0d9949222e6eb1366fa5e2a70df
Beatriz <bia_nonato@icloud.com>
2018-09-25 22:23:50 -0300

    trocar console.log por alert

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let escolha;
 while(escolha !== segredo){ // !== diferente
     escolha = Number(prompt("Digite um número:"));
     if(escolha > segredo){
-        console.log("ERROU! O número era menor. Tente outra vez!")
+        alert("ERROU! O número era menor. Tente outra vez!")
     }
     else if(escolha < segredo){
-        console.log("ERROU! O número era maior. Tente outra vez!")
+        alert("ERROU! O número era maior. Tente outra vez!")
     }
     else{
-        console.log("ACERTOU!!!")
+        alert("ACERTOU!!!")
     }
 }
